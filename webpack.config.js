b2f7c929319c42a59683268c21a8ac108290626f
PlayMa256 <matheus.g.silva@hotmail.com>
2019-05-14 14:31:46 -0300

    add alias

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = {
 	output: {
 		filename: "bundle.js"
 	},
+	resolve: {
+		alias: {
+			"@babel/loader": require.resolve(path.resolve(__dirname, "node_modules/@babel/core/"))
+		}
+	},
 	resolveLoader: {
 		modules: [path.resolve(__dirname, "node_modules")]
 	},
